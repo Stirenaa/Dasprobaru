f30abb941623844379648ee445c0da1e85b47378
Stirenaa <abdiraihan100@gmail.com>
2023-10-11 13:04:50 +0700

    revisi

--- Praktikum06/Kabisat25.java
+++ Praktikum06/Kabisat25.java
@@ -7,16 +7,14 @@ public class Kabisat25 {
         System.out.println("Masukkan Tahun");
         tahun = sc.nextInt();
         
-        if ((tahun & 400) == 0 || (tahun % 100) != 0){
-            System.out.println("Tahun Kabisat");
-        }else if ((tahun & 400) == 0)
-            System.out.println("Bukan Tahun Kabisat");
+        if ((tahun % 4) == 0 && (tahun % 100) != 0 || (tahun % 400) == 0) {
+            System.out.println("Tahun Kabisat");}
         else {
             System.out.println( "Bukan Tahun Kabisat");
-             }
+            }
     }
 }
-     
+
     
     
 
