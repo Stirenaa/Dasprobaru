29c6d5222849989a996d5e18ba2299e0f71e512c
Stirenaa <abdiraihan100@gmail.com>
2023-10-11 11:45:02 +0700

    revisi

--- Praktikum06/Kabisat25.java
+++ Praktikum06/Kabisat25.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 public class Kabisat25 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         int tahun;
         System.out.println("Masukkan Tahun");
         tahun = sc.nextInt();
         
-        if ((tahun & 4) == 0 && (tahun % 100) != 0){
+        if ((tahun & 400) == 0 || (tahun % 100) != 0){
             System.out.println("Tahun Kabisat");
-        } else {
+        }else if ((tahun & 400) == 0)
+            System.out.println("Bukan Tahun Kabisat");
+        else {
             System.out.println( "Bukan Tahun Kabisat");
-        }
+             }
     }
 }
+     
+    
+    
+
