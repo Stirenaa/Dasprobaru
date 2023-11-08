091296f3c0e17a282d6f67509e9df9939ffc511a
Stirenaa <abdiraihan100@gmail.com>
2023-11-08 13:27:51 +0700

    Soal nomor 11

--- Praktikum10/Bioskop25.java
+++ Praktikum10/Bioskop25.java
@@ -30,9 +30,14 @@ public class Bioskop25 {
         // for (String[] barisPenonton : penonton){
         //      System.out.println("Panjang Baris " +barisPenonton.length);
         
-        System.out.println("Penonton pada baris ke-3 : "  );
-        for (int i = 0; i < penonton[2].length; i++){
-            System.out.println(penonton[2][i]);
+        // System.out.println("Penonton pada baris ke-3 : "  );
+        // for (int i = 0; i < penonton[2].length; i++){
+        //     System.out.println(penonton[2][i]);
+        // }
+
+        System.out.println("Penonton pada baris ke-3: ");
+        for (String i : penonton[2]){
+            System.out.println(i);
         }
         
     }
