07226fc7904556aa355825f3c460abb8e888df17
Stirenaa <abdiraihan100@gmail.com>
2023-11-08 13:33:27 +0700

    Revisi final

--- Praktikum10/Bioskop25.java
+++ Praktikum10/Bioskop25.java
@@ -24,22 +24,27 @@ public class Bioskop25 {
         // System.out.println(penonton[2].length);
         // System.out.println(penonton[3].length);
        
+        //Soal Nomor 5
         //for (int i = 0; i < penonton.length; i++){
         //      System.out.println("Panjang Baris Ke-" + (i+1) + penonton[i].length);
         
+        //Soal Nomor 6
         // for (String[] barisPenonton : penonton){
         //      System.out.println("Panjang Baris " +barisPenonton.length);
         
+        //Soal Nomor 10
         // System.out.println("Penonton pada baris ke-3 : "  );
         // for (int i = 0; i < penonton[2].length; i++){
         //     System.out.println(penonton[2][i]);
         // }
 
+        //Soal Nomor 11
         // System.out.println("Penonton pada baris ke-3: ");
         // for (String i : penonton[2]){
         //     System.out.println(i);
         // }
 
+        //Soal Nomor 12
         for (int i = 0; i < penonton.length; i++){
             System.out.println("Penonton pada baris ke-" + (i+1) + ": " +String.join(",",penonton[i]));
         }
