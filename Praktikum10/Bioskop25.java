66bf2b7270927e089448186e26890e78c34ca1f1
Stirenaa <abdiraihan100@gmail.com>
2023-11-08 13:21:06 +0700

    pertanyaan 1-4

--- Praktikum10/Bioskop25.java
+++ Praktikum10/Bioskop25.java
@@ -11,6 +11,7 @@ public class Bioskop25 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
 
         System.out.printf("%s \t %s\n", penonton [0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton [1][0], penonton[1][1]);
@@ -18,14 +19,15 @@ public class Bioskop25 {
         System.out.printf("%s \t %s\n", penonton [3][0], penonton[3][1]);
     
         System.out.println(penonton.length);
-        // System.out.println(penonton[0].length);
-        // System.out.println(penonton[1].length);
-        // System.out.println(penonton[2].length);
-        // System.out.println(penonton[3].length);
-        for (int i = 0; i < penonton.length; i++){
-            System.out.println("Panjang Baris Ke-" + (i+1) + penonton[i].length);
-        }
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);
+       
+
+        
         
     }
-
+        
 }
+
