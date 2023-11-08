04aec859c215c3272f32a46380d99f833332d33f
Stirenaa <abdiraihan100@gmail.com>
2023-11-08 13:12:02 +0700

    hehehehehe

---
+++ Praktikum10/Bioskop25.java
@@ -0,0 +1,31 @@
+//import java.util.Scanner;
+public class Bioskop25 {
+    public static void main(String[] args) {
+        //Scanner input = new Scanner(System.in);
+
+        String[][] penonton = new String[4][2];
+        penonton[0][0] = "amir";
+        penonton[0][1] = "Benny";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+
+        System.out.printf("%s \t %s\n", penonton [0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton [1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton [2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton [3][0], penonton[3][1]);
+    
+        System.out.println(penonton.length);
+        // System.out.println(penonton[0].length);
+        // System.out.println(penonton[1].length);
+        // System.out.println(penonton[2].length);
+        // System.out.println(penonton[3].length);
+        for (int i = 0; i < penonton.length; i++){
+            System.out.println("Panjang Baris Ke-" + (i+1) + penonton[i].length);
+        }
+        
+    }
+
+}
