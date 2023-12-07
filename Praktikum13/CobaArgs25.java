31f4f61971a411ded839e4d1c6e3c8ebf0cd0853
Stirenaa <abdiraihan100@gmail.com>
2023-12-08 00:07:00 +0700

    hehe

---
+++ Praktikum13/CobaArgs25.java
@@ -0,0 +1,10 @@
+public class CobaArgs25 {
+    public static void main(String[] args) {
+        System.out.println("Haii Beb " + args[0]);
+        System.out.println("Selamat Datang di " + args[1]);
+
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
---
+++ Praktikum13/LocalGlobal25.java
@@ -0,0 +1,24 @@
+public class LocalGlobal25 {
+    static int a = 10;
+    public static void main(String[] args) {
+        int b = 10;
+        int c = 5;
+        a = a + 8;
+
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+        System.out.println("Nilai b: " + c);
+
+        display();
+    }
+    private static void display() {
+        int b = 10;
+        int c = 5;
+        a = a + 12;
+
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+        System.out.println("Nilai c: " + c);
+
+    }
+}
